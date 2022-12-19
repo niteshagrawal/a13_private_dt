db33d5ed35d2885e34fe81e8bf15e8a2fb4c7010
niteshagrawal <niteshgupta.ng938@gmail.com>
2022-12-19 18:25:19 +0530

    violet: Ricedroid initial bring up

--- lineage_violet.mk
+++ lineage_violet.mk
@@ -9,13 +9,22 @@
 $(call inherit-product, device/xiaomi/violet/device.mk)
 
 # Inherit some common SuperiorOS stuff.
-$(call inherit-product, vendor/superior/config/common.mk)
-SUPERIOR_OFFICIAL := true
-TARGET_BOOT_ANIMATION_RES := 1080
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Bootanimation Resolution
+SUSHI_BOOTANIMATION := 1080
+
+# Charging Animation
 TARGET_INCLUDE_PIXEL_CHARGER := true
 
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_BUILD_GRAPHENEOS_CAMERA := false
+TARGET_ENABLE_BLUR := true
+TARGET_USE_PIXEL_FINGERPRINT := true
+
+
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := superior_violet
+PRODUCT_NAME := lineage_violet
 PRODUCT_DEVICE := violet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 7 Pro
@@ -25,3 +34,11 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="violet"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# RiceDroid Flags
+RICE_MAINTAINER := AssGuardGod
+RICE_DEVICE := violet
+RICE_OFFICIAL := true
+RICE_CHIPSET := sm6150
+WITH_GMS := true
+TARGET_OPTOUT_GOOGLE_TELEPHONY := true
